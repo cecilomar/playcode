8213538490bdb6ca78b810c4808bc0a1b1ede070
Cecil O. Almonte <info@cecilomar.com>
2014-09-23 21:54:47 -0500

    Added Banner to playcode.php
    Just a description this and that.

---
+++ cecilomar_playcode/playcode.php
@@ -0,0 +1,11 @@
+<?php
+
+/* @project		Cecilomar PlayCode
+** @author		Cecil O. Almonte <info@cecilomar.com>
+** @copyright	2014 Cecilomar Design, Inc.
+** @license		GNU GENERAL PUBLIC LICENSE Version 3 (GPL V3)
+** @git			https://github.com/cecilomar/playcode/
+** @file		https://github.com/cecilomar/playcode/blob/master/cecilomar_playcode/playcode.php
+**/
+
+?>
